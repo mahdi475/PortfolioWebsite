200d9b219ad51b1920775cb4e1a71d74de0b5655
Mahdi Mosavi <mahdimousavi8909@gmail.com>
2025-08-22 18:51:58 +0200

    React vite template Start

---
+++ my-app/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Topbar from './topbar.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Topbar />
+  </StrictMode>,
+)
---
+++ my-app/src/topbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './topbar.css'
+
+function topbar() {
+  return (
+    <>
+      <div className='topBar'>
+        <p>hello</p>
+      </div>
+    </>
+  )
+}
+
+export default topbar
